e7f85298c524ca9b8088fa4d1e05bc985bb8af76
Гаяне <gayanev.stepanyan@yandex.ru>
2024-01-17 14:21:06 +0300

    added some explanations to the tasks

--- homeWork_01.js
+++ homeWork_01.js
@@ -1,50 +1,66 @@
-//Задание 1
+//Задание 1 - Создать объект counter всеми возможными способами;
 
+// 1) литеральная форма:
 const counter = {
   firstName: 'Gaya',
   lastName: 'Orlova',
 };
-
+// 2) с помощью ключевого слова new:
 const counter1 = new Object();
-const counter2 = Object.create({});
+counter1.firstName = 'Gaya';
+counter1.lastName = 'Orlova';
+
+// 3) с помощью метода Object.create(), который создает новый объект,
+// используя существующий объект в качестве прототипа вновь созданного объекта:
+const counter2 = Object.create({}, {
+  name: {value: 'Gaya'},
+  sirname: {value: 'Orlova'}
+});
 
 
-// Задание 2
+// Задание 2 - Скопировать объект counter всеми возможными способами
 // 1.
-const newObj = Object.assign({}, counter);
+const newObj = Object.assign({}, counter); //неглубокое копирование
 
 // 2.
-const newObj1 = {...counter};
+const newObj1 = {...counter}; //неглубокое копирование
 
 // 3.
 const newObj2 = JSON.parse(JSON.stringify(counter));
+// если объект содержит функции или свойства, которые не могут быть сериализованы в JSON формат,
+// или если он содержит свойства с ссылками на другие объекты или циклические ссылки,
+// то использование такого способа для глубокого копирования может привести к непредсказуемым результатам. 
 
-// 4.
-const newObj3 = structuredClone(counter);
-
-// 5.
-import cloneDeep from 'lodash/cloneDeep'
-const newObj4 = _.cloneDeep(counter);
-
-// 6. 
+// 4. 
 const newObj5 = {};
 for (let key in counter) {
   newObj[key] = [counter.key];
-};
+}; //неглубокое копирование
+
+// 5.
+const newObj3 = structuredClone(counter); // глубокое копирование
+
+// 6. //с помощью библиотек, например:
+import cloneDeep from 'lodash/cloneDeep' 
+const newObj4 = _.cloneDeep(counter); //глубокое копирование
+
+// 7. самостоятельно написанная функция глубокого копирования 
 
 
-// Задание 3
+
+// Задание 3 - Создать функцию makeCounter всеми описанными и возможными способами
+
 const makeCounter = () => {
   return console.log('Happy New Year');
-};
+}; // стрелочная функция
 
 function makeCounter1 () {
   return console.log('Happy New Year');
-};
+}; // Function Declaration 
 
 const makeCounter2 = function () {
   return console.log('Happy New Year');
-}
+}; // Function Expression
 
 const makeCounter3 = function makeCounterFunction(value) {
   if (value) {
@@ -52,11 +68,11 @@ const makeCounter3 = function makeCounterFunction(value) {
   } else {
     makeCounterFunction('no value');
   }
-};
+};// named function expressions
 
 
 
-// Доп.задание 1.
+// Доп.задание 1. Написать функцию глубокого сравнения двух объектов:
 
 const obj1 = {
   here: {
@@ -97,14 +113,18 @@ const deepEqual = (obj1, obj2) => {
   }
   return 'Objects are equal';
 };
+// Проверяем, являются ли оба аргумента объектами,
+// затем проверяем, имеют ли они одинаковое количество свойств(длину).
+// Затем функция рекурсивно вызывает саму себя для каждого свойства объекта.
+// Если все свойства равны, функция возвращает true. Если хотя бы одно свойство не равно, функция возвращает false.
 
 
-// Доп.задание 2.
+// Доп.задание 2. Развернуть строку в обратном направлении при помощи методов массивов:
 function reverseStr(str) {
   return str.split('').reverse().join('');
 }
 
-//Доп.задание 3
+//Доп.задание 3 - из лекции
 
 const arr = +[6]
 console.log(arr) //6
